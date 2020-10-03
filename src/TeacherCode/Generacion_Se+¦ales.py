dfd97ea4f1bb483c6a001e2cac8240e2cb948b24
santiago <jsbarcenas00@gmail.com>
2020-10-02 21:12:26 -0500

    salvand ss

--- src/TeacherCode/Generacion_Se+¦ales.py
+++ src/TeacherCode/Generacion_Se+¦ales.py
@@ -1,50 +1,58 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-t_ini = -1
-t_fin = 1
-dt = 0.001
-t = np.arange(t_ini,t_fin,dt) # VECTOR DE TIEMPO
+# u0 = np.piecewise(t,t>=-0.3,[2,1])  #SEÑAL ESCALÓN
+# plt.figure(1)
+# plt.plot(t,u0)
+# plt.xlabel('t')
+# plt.ylabel('escalon u(t)')
+# plt.show()
 
-y = np.sin(2*np.pi*t)  # SEÑAL TRIGONOMÉTRICA
+
+# imp = 0
+# delta = np.piecewise(t,t>=0,[1,0])
+# delta_dt = np.piecewise(t,t>=imp+dt,[1,0])
+# unimpulso = delta - delta_dt      #SEÑAL IMPULSO
+# plt.figure(2)
+# plt.plot(t,unimpulso)
+# plt.show()
+
+
+
+
+
+# from scipy import signal as sp
+# t1 = np.arange(0,10, 0.001)
+# w = sp.square(t1)    #SEÑAL CUADRADA
+# plt.plot(t1,w)
+# plt.show()
+
+# x = sp.sawtooth(t1) #SEÑAL TRIANGULAR
+# plt.plot(t1,x) ## Sirve para graficar una función sobre otra
+# plt.show()
+# h = np.arange((min(t1)+ min(t1)), (max(t1) + max(t1)))
+# plt.plot(h, np.convolve(w,x))
+# plt.title('lafsdafsd')
+# plt.show()
+
+
+dt = 0.001
+t = np.arange(0,1,dt) # VECTOR DE TIEMPO
+
+t2 = np.arange(0,2,0.001)
+y = np.sin(2*np.pi*t)*6  # SEÑAL TRIGONOMÉTRICA
 plt.plot(t,y)
 plt.title('Gráfica')
 plt.show()
 
-u0 = np.piecewise(t,t>=-0.3,[2,1])  #SEÑAL ESCALÓN
-plt.figure(1)
-plt.plot(t,u0)
-plt.xlabel('t')
-plt.ylabel('escalon u(t)')
+z = np.exp(t2)  #SEÑAL EXPONENCIAL
+plt.plot(t2,z)
 plt.show()
 
-
-imp = 0
-delta = np.piecewise(t,t>=0,[1,0])
-delta_dt = np.piecewise(t,t>=imp+dt,[1,0])
-unimpulso = delta - delta_dt      #SEÑAL IMPULSO
-plt.figure(2)
-plt.plot(t,unimpulso)
+tc = max(t)+ max(t2)
+tl = min(t) + min(t2)
+dss = 0.001
+t3 = np.arange(tl,tc+dss,dss)
+conv = np.convolve(z,y)
+plt.plot(t3, conv)
 plt.show()
-
-
-z = np.exp(t)  #SEÑAL EXPONENCIAL
-plt.plot(t,z)
-plt.show()
-
-
-from scipy import signal as sp
-t1 = np.arange(0,10, 0.001)
-w = sp.square(t1)    #SEÑAL CUADRADA
-plt.plot(t1,w)
-plt.show()
-
-x = sp.sawtooth(t1) #SEÑAL TRIANGULAR
-plt.plot(t1,x) ## Sirve para graficar una función sobre otra
-plt.show()
-h = np.arange((min(t1)+ min(t1)), (max(t1) + max(t1)))
-plt.plot(h, np.convolve(w,x))
-plt.title('lafsdafsd')
-plt.show()
-
-
--- src/TeacherCode/labels.py
+++ src/TeacherCode/labels.py
@@ -1,76 +1,143 @@
-# Graficas de funciones singulares
-# from matplotlib.pyplot import *
-# from numpy import *
+# # Graficas de funciones singulares
+# # from matplotlib.pyplot import *
+# # from numpy import *
 
-# # funcion escalon unitario
-# def u(t): return piecewise(t,[t<0.0,t>=0.0],[0,1])
+# # # funcion escalon unitario
+# # def u(t): return piecewise(t,[t<0.0,t>=0.0],[0,1])
 
-# # funcion f(t)
-# def f(t): return t*(u(t)-u(t-1))+2*u(t-1)
+# # # funcion f(t)
+# # def f(t): return t*(u(t)-u(t-1))+2*u(t-1)
  
-# t=linspace(-1,5,1000)
-# plot(t,f(t),'k',label=r'$f(t)$',lw=2)
-# xlim(-1,3);ylim(0,2.2)
-# legend(loc='best')
-# title(r'Funci\'on $f(t)=tu(t)-(t-1)u(t-1)+u(t-1)$')
-# show()
+# # t=linspace(-1,5,1000)
+# # plot(t,f(t),'k',label=r'$f(t)$',lw=2)
+# # xlim(-1,3);ylim(0,2.2)
+# # legend(loc='best')
+# # title(r'Funci\'on $f(t)=tu(t)-(t-1)u(t-1)+u(t-1)$')
+# # show()
 
+# # import numpy as np
+# # from matplotlib import pyplot as plt
+# # def ut(t):
+# #  return t*np.arange(0,5)
+
+
+# # t = np.arange(0,10,0.001)
+# # f= np.piecewise(t, t>0, [ut(5),0])
+# # plt.plot(t,f)
+# # plt.show()
+
+# # Señales modelo varias
 # import numpy as np
 # from matplotlib import pyplot as plt
-# def ut(t):
-#  return t*np.arange(0,5)
+# from scipy import signal as sp
+
+# # tf = 6
+# # ti = -8
+# # tk = tf -ti
 
 
-# t = np.arange(0,10,0.001)
-# f= np.piecewise(t, t>0, [ut(5),0])
-# plt.plot(t,f)
-# plt.show()
-
-# Señales modelo varias
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy import signal as sp
-
-tf = 10
-ti = 3
-td = tf -ti
-tk = (td-ti)/2
-tl = tk+ti
-print(tk)
-t=np.arange(ti,tf, 0.001)
-y = np.piecewise(t,t>0,[1])
-def u(t): return np.piecewise(t,[t<0.0,t>=0.0,],[0,1])
-def f(t): return  (t-tk)*(u(t-tk)-u(t-tl))+ti*u(t-tl) 
-plt.plot(t, f(t))
-plt.show()
+# # t=np.arange(ti,tf, 0.001)
+# # y = np.piecewise(t,t>0,[1])
+# # def u(t): return np.piecewise(t,[t<0.0,t>=0.0,],[0,1])
+# # def f(t): return  (t-(tk/3 + (ti)))*(u(t-(tk/3+(ti)))-u(t-(tk*2/3 + (ti))))+(tk/3)*u(t-(tk*2/3 + (ti))) 
+# # plt.plot(t, f(t))
+# # plt.show()
 
 
-# tf = 30
+# # tf = 30
+# # ti = -22
+
+# # tk = tf -ti
+
+# # print(tk)
+# # t=np.arange(ti,tf, 0.001)
+# # y = np.piecewise(t,t>0,[1])
+# # def u(t): return np.piecewise(t,[t<0.0,t>=0.0,],[0,1])
+# # def f(t): return  np.flip ((t-(tk/3 + (ti)))*(u(t-(tk/3+(ti)))-u(t-(tk*2/3 + (ti))))+(tk/3)*u(t-(tk*2/3 + (ti))) )
+# # plt.plot(t, f(t))
+# # plt.show()
+
+
+
+
+
+
+# tf = 4
 # ti = 0
 # td = tf -ti
-# tk = (td-3)/2
-# tl = tk+3
-# tk2 = tl+5.5
+# tk = 0
+# tl = 3  
+# tk2 = tl+4
 # tl2= tk2 +3
 # print(tk)
 # t=np.arange(ti,tf, 0.001)
 # y = np.piecewise(t,t>0,[1])
 # def u(t): return np.piecewise(t,[t<0.0,t>=0.0,],[0,1])
-# def f(t): return  np.flip((t-tk)*(u(t-tk)-u(t-tl))+3*u(t-tl) )
+# def f(t): return  (t)*(u(t)-u(t-tl))+tl*u(t-tl)          -         (t-(tf-3))*(u(t-(tf-3))-u(t-(tf)))-3*u(t-(tf))
 # plt.plot(t, f(t))
 # plt.show()
 
 
-tf = 20
-ti = 5
 
-tl = 3  
-tk2 = tl+4
-tl2= tk2 +3
-print(tk)
-t=np.arange(ti,tf, 0.001)
-y = np.piecewise(t,t>0,[1])
-def u(t): return np.piecewise(t,[t<0.0,t>=0.0,],[0,1])
-def f(t): return  (t-ti)*(u(t-ti)-u(t-(ti+tl)))+tl*u(t-(ti+tl)) - (t-(tf-3))*(u(t-(tf-3))-u(t-(tf)))-3*u(t-(tf))
-plt.plot(t, f(t))
+# t = np.arange(-10,10,0.001)
+
+# ge =-1* (2*(t**2))+t*2
+
+# plt.plot(t,np.flip(ge))
+# plt.show()
+
+import scipy.integrate
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def showConvolution(f1, f2, t0):
+    # Calculate the overall convolution result using Simpson integration
+    convolution = np.zeros(len(t))
+    for n, t_ in enumerate(t):
+        prod = lambda tau: f1(tau) * f2(t_-tau)
+        convolution[n] = scipy.integrate.simps(prod(t), t)
+
+    # Create the shifted and flipped function
+    f_shift = lambda t: f2(t0-t)
+    prod = lambda tau: f1(tau) * f2(t0-tau)
+
+    # Plot the curves
+    plt.gcf().clear() # il
+
+    plt.subplot(211)
+    plt.gca().set_ymargin(0.05) # il
+    plt.plot(t, f1(t), label=r'$f_1(\tau)$')
+    plt.plot(t, f_shift(t), label=r'$f_2(t_0-\tau)$')
+    plt.fill(t, prod(t), color='r', alpha=0.5, edgecolor='black', hatch='//') # il
+    plt.plot(t, prod(t), 'r-', label=r'$f_1(\tau)f_2(t_0-\tau)$')
+    plt.grid(True); plt.xlabel(r'$\tau$'); plt.ylabel(r'$x(\tau)$') # il
+    plt.legend(fontsize=10) # il
+    plt.text(-4, 0.6, '$t_0=%.2f$' % t0, bbox=dict(fc='white')) # il
+
+    # plot the convolution curve
+    plt.subplot(212)
+    plt.gca().set_ymargin(0.05) # il
+    plt.plot(t, convolution, label='$(f_1*f_2)(t)$')
+
+    # recalculate the value of the convolution integral at the current time-shift t0
+    current_value = scipy.integrate.simps(prod(t), t)
+    plt.plot(t0, current_value, 'ro')  # plot the point
+    plt.grid(True); plt.xlabel('$t$'); plt.ylabel('$(f_1*f_2)(t)$') # il
+    plt.legend(fontsize=10) # il
+    plt.show() # il
+
+Fs = 50  # our sampling frequency for the plotting
+T = 5    # the time range we are interested in
+t = np.arange(-T, T, 1/Fs)  # the time samples
+f1 = lambda t: np.maximum(0, 1-abs(t))
+f2 = lambda t: (t>0) * np.exp(-2*t)
+
+t0 = np.arange(-2.0,2.0, 0.05)
+
+fig = plt.figure(figsize=(8,3))
+anim = animation.FuncAnimation(fig, showConvolution(f1,f2, t0), frames=np.linspace(0, 50, 500), interval=80)
+
+anim.save('animation.mp4', fps=30) # fps = frames per second
+
 plt.show()
